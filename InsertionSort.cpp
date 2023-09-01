221591e302de8c5cdaf638aa7fbb9dd9b3862a9e
musharraf47 <musharrafm9972@gmail.com>
2023-09-02 00:38:19 +0530

    Sorting-02

---
+++ InsertionSort.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int j;
+    for (int i = 1; i < n; i++)
+    {
+        int temp = a[i];
+        for (j = i - 1; j >= 0; j--)
+        {
+            if (a[j] > temp)
+                a[j + 1] = a[j];
+
+            else
+                break;
+        }
+        a[j + 1] = temp;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+}
